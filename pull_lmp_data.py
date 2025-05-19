3b3b3eb4301af04072b5783d0367c60bdf68c726
Angel Salazar <aangelsalazarr@gmail.com>
2025-05-18 21:13:50 -0500

    successfully pulled in all lmp data frmo 7 isos

--- pull_lmp_data.py
+++ pull_lmp_data.py
@@ -9,6 +9,14 @@ from private_info import spp_functions as sfuncs
 # now we are loading in from nyiso pull function
 from private_info import nyiso_functions as nfuncs
 
+# loading in from caiso pull function
+from private_info import caiso_functions as cfuncs
+from private_info import caiso_api_struct as cas
+
+# loading in from isone pull function 
+from private_info import isone_functions as ifuncs
+import polars as pl
+
 
 # let's set up a way to define what date dataset we want to pulll from 
 from datetime import date, datetime, timedelta
@@ -28,22 +36,43 @@ ercot_client = efuncs.ERCOT_LMP_Client(
     emil_id=eas.lmp_by_node['emil_id'], 
     time_from_id=eas.lmp_by_node['time_start_id'], 
     time_to_id=eas.lmp_by_node['time_end_id'], 
-    topic_id=eas.lmp_by_node['topic_id']
+    topic_id=eas.lmp_by_node['topic_id'], 
+    choose_date=None
 )
 
-# initializing the class
-# ercot_client.get_lmp_data()
+# initializing the class for ercot
+ercot_client.get_lmp_data()
 
 
 # setting up SPP function pull
-##sfuncs.pull_spp_lmp(choose_date="20250514", topic_id='LMP_By_SETTLEMENT_LOC', file_type='csv', 
- ##            save_path="datasets/spp_lmp_data")
+sfuncs.pull_spp_lmp(choose_date="20250517", topic_id='LMP_By_SETTLEMENT_LOC', file_type='csv', 
+            save_path="datasets/spp_lmp_data")
 
 
 # setting up NYISO function pull
-##nfuncs.grab_nyiso_daily_lbmp(date_pull="20250514", save_path="datasets/nyiso_lmp_data", 
-##                      topic_id="realtime_zone")
+nfuncs.grab_nyiso_daily_lbmp(date_pull="20250517", save_path="datasets/nyiso_lmp_data", 
+                      topic_id="realtime_zone")
 
 
+# setting up ISONE function pull
+# need to first load in our csv file with hubs/node ids
+local_isone_hubs = pl.read_csv('private_info/isone_lmp_locations.csv')
+ifuncs.aggregate_isone_lmps(locations=local_isone_hubs, begin_date=f"20250517",
+                     auth_uname=creds.isone_creds['username'], 
+                     auth_pword=creds.isone_creds['password'], 
+                     topic_id="fiveminutelmp",
+                     save_path="datasets/isone_lmp_data"
+                                     )
 
 
+# setting up function to pull from caiso as well
+caiso_client = cfuncs.CAISO_LMP_Client(
+    topic_id=cas.fivemin_lmp['topic_id'], 
+    choose_date='20250517', 
+    save_folder='datasets/caiso_lmp_data/lmp_data_zip', 
+    xml_dest='datasets/caiso_lmp_data/lmp_data_xml', 
+    save_path='datasets/caiso_lmp_data', 
+    csv_path='datasets/caiso_lmp_data/lmp_csv'
+)
+
+caiso_client.get_lmp_data()
